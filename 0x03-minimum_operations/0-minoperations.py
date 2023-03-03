a7cdb1c4c5f4835d3e348d2f2c0430b80f6db91b
lilisep <lilianasepulveda13@gmail.com>
2023-03-03 10:48:57 -0500

    add file

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""documentation"""
+import math
+
+
+def minOperations(n):
+    """documentation"""
+    res = 0
+    if n > 1:
+        for i in range(2, int(math.sqrt(n) + 1)):
+            while n % i == 0:
+                res += i
+                n //= i
+    return res + n if n > 1 else res
+    
